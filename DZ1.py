ccb2b17beb605d96f3692e2a40efaf2a92dd2f35
Антон Федотов <anthony.fedotos@gmail.com>
2023-07-05 12:39:13 +0600

    Creaty DZ1

---
+++ DZ1.py
@@ -0,0 +1,39 @@
+
+#Exercise 1
+
+# print('Enter first number:')
+# first = input()
+# print('Enter second number:')
+# second = input()
+# print('Enter third number:')
+# third = input()
+
+# sum_numbers = first + second + third
+# print('Sum of numbers: ' + sum_numbers)
+
+#Exercise 2
+
+# salary = int(input('Enter monthly salary: '))
+# credit = int(input('Enter credit payment: '))
+# bill = int(input('Enter utility bill: '))
+
+# cash_balance = salary - credit - bill
+# print('Cash balance: '+ str(cash_balance)) 
+
+#Exercise 3
+
+# diagonal_A = int(input('Enter diagonal A: '))
+# diagonal_B = int(input('Enter diagonal B: '))
+
+# S_Rhombus = (diagonal_A * diagonal_B) / 2
+# print(S_Rhombus)
+
+#Exercise 4
+
+# new_string = "\n To be \n or not \n to be" 
+# print(new_string)
+
+#Exercise 5
+
+# new_string = '"Life is what happenes \n   when \n     you\'re busy making other plans"' + '\n' + '                                 ' + 'John Lennon.'
+# print(new_string)
