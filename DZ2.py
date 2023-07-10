6bab67a7047dc66ec61ff7f44f5b2ce36e9dd5d9
Антон Федотов <anthony.fedotos@gmail.com>
2023-07-10 18:59:49 +0600

    Create DZ2.py
    add task 2

---
+++ DZ2.py
@@ -0,0 +1,70 @@
+# Exercise 1
+
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# num3 = int(input("Enter the third number: "))
+
+# choice = input("Enter 'sum' to display the sum or 'p' to display the product: ")
+
+# if choice == 's':
+#     print(f"The sum of the three numbers is {num1 + num2 + num3}")
+# elif choice == 'p':
+#     print(f"The product of the three numbers is {num1 * num2 * num3}")
+# else:
+#     print("Invalid choice")
+
+#Exercise 2
+
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# num3 = int(input("Enter the third number: "))
+
+# x = num1
+# if num2 > x:
+#     x = num2
+# if num3 > x:
+#     x = num3
+
+# y = num1
+# if num2 < x:
+#     x = num2
+# if num3 < x:
+#     x = num3
+
+# av = (num1 + num2 + num3) // 3
+
+# choice = input("Enter 'max' to display the sum or 'min' to display the product or 'average' to display average: ")
+
+# if choice == 'max':
+#     print(f"The max of the three numbers is " + str(x))
+# elif choice == 'min':
+#     print(f"The min of the three numbers is " + str(y))
+# elif choice == 'average':
+#     print(f"The average of the three numbers is " + str(av))
+# else:
+#     print("Invalid choice")
+
+
+#Exercise 3
+
+# meters = float(input("Enter the number of meters: "))
+
+# miles = (meters / 1609)
+# dume = (meters * 39,37)
+# yards = (meters * 1.094)
+
+# choice = input("Enter 'miles' to display the miles or 'dume' to display the dume or 'yards' to display yards: ")
+
+# if choice == 'miles':
+#     print(f"Miles = " + str(miles))
+# elif choice == 'dume':
+#      print(f"Dume = " + str(dume))
+# elif choice == 'average':
+#      print(f"Yards = " + str(yards))
+# else:
+#      print("Invalid choice")
+
+
+
+
+
